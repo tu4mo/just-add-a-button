a11f3c395ed246e1f7bff9ab9fa4705bb92ff895
tu4mo <tu4mo@users.noreply.github.com>
2020-12-19 21:31:46 +0200

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,9 @@ module.exports = {
           {
             loader: "postcss-loader",
             options: {
-              ident: "postcss",
-              plugins: [require("tailwindcss"), require("autoprefixer")],
+              postcssOptions: {
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+              },
             },
           },
         ],
