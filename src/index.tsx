06b37123455eef2b2b5d28fbb42681aaefc84dce
tu4mo <tu4mo@users.noreply.github.com>
2021-06-14 16:14:56 +0300

    chore: update to React 18 alpha

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,10 @@ import store from "./store";
 import App from "./App";
 import "./styles.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
