d8478ad55301c07cbe7726a8eec2662e9551faf2
tu4mo <tu4mo@users.noreply.github.com>
2024-03-06 11:40:47 +0200

    Use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { Suspense } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { View } from "./View";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <View />,
